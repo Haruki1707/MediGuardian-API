8f24fa363020970d1511e28b4ed2199d4ff598d3
Haruki1707 <72423267+Haruki1707@users.noreply.github.com>
2023-11-20 21:46:09 -0600

    Login and logout added

---
+++ app/Http/Controllers/AuthenticatedController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthenticatedController extends Controller
+{
+    /**
+     * Authenticates the user and generates a new token.
+     */
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'email' => 'required|email|max:100',
+            'password' => 'required|string|max:100',
+        ]);
+
+        if (!auth()->attempt($attributes)) {
+            return response()->json([
+                'message' => 'Invalid email or password'
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Successfully logged in',
+            'user' => $request->user()->createToken($request->get('email'))->plainTextToken
+        ]);
+    }
+
+    /**
+     * Destroys the token for the authenticated user.
+     */
+    public function destroy(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticatedController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('login', [AuthenticatedController::class, 'store'])->name('login');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthenticatedController::class, 'destroy'])->name('logout');
+});
+
+Route::get('ziggy', fn() => response()->json(new \Tightenco\Ziggy\Ziggy()));
