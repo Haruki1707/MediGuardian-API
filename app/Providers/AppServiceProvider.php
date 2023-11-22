5238f24debdb79e3a2974196206e1c93159b2e43
Haruki1707 <72423267+Haruki1707@users.noreply.github.com>
2023-11-22 03:18:03 -0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\DaysSchedule;
+use App\Models\HoursSchedule;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Relation::morphMap([
+            'HoursSchedule' => HoursSchedule::class,
+            'DaysSchedule' => DaysSchedule::class,
+        ]);
     }
 }
