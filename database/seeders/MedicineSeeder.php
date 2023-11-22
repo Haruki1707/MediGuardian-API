d6f20e5f6e1cd10ff0bec20bdef6e724ea774fc1
Haruki1707 <72423267+Haruki1707@users.noreply.github.com>
2023-11-22 03:17:46 -0600

    Medicine Seeder

--- database/seeders/MedicineSeeder.php
+++ database/seeders/MedicineSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Medicine;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,63 @@ class MedicineSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $medicines = [
+            ['name' => 'Aspirin'],
+            ['name' => 'Ibuprofen'],
+            ['name' => 'Paracetamol (Acetaminophen)'],
+            ['name' => 'Lisinopril'],
+            ['name' => 'Simvastatin'],
+            ['name' => 'Metformin'],
+            ['name' => 'Omeprazole'],
+            ['name' => 'Levothyroxine'],
+            ['name' => 'Amoxicillin'],
+            ['name' => 'Ciprofloxacin'],
+            ['name' => 'Fluoxetine'],
+            ['name' => 'Losartan'],
+            ['name' => 'Atorvastatin'],
+            ['name' => 'Metoprolol'],
+            ['name' => 'Prednisone'],
+            ['name' => 'Warfarin'],
+            ['name' => 'Diazepam'],
+            ['name' => 'Furosemide'],
+            ['name' => 'Hydrochlorothiazide'],
+            ['name' => 'Naproxen'],
+            ['name' => 'Cetirizine'],
+            ['name' => 'Albuterol'],
+            ['name' => 'Dextromethorphan'],
+            ['name' => 'Nexium'],
+            ['name' => 'Zolpidem'],
+            ['name' => 'Venlafaxine'],
+            ['name' => 'Mirtazapine'],
+            ['name' => 'Gabapentin'],
+            ['name' => 'Ranitidine'],
+            ['name' => 'Amlodipine'],
+            ['name' => 'Celecoxib'],
+            ['name' => 'Clonazepam'],
+            ['name' => 'Doxycycline'],
+            ['name' => 'Escitalopram'],
+            ['name' => 'Folic Acid'],
+            ['name' => 'Hydroxyzine'],
+            ['name' => 'Jantoven'],
+            ['name' => 'Ketoconazole'],
+            ['name' => 'Lorazepam'],
+            ['name' => 'Meloxicam'],
+            ['name' => 'Nortriptyline'],
+            ['name' => 'Oxycodone'],
+            ['name' => 'Pantoprazole'],
+            ['name' => 'Quetiapine'],
+            ['name' => 'Risperidone'],
+            ['name' => 'Sertraline'],
+            ['name' => 'Trazodone'],
+            ['name' => 'Ursodiol'],
+            ['name' => 'Vitamin D'],
+            ['name' => 'Warfarin'],
+            ['name' => 'Xanax'],
+            ['name' => 'Yaz'],
+            ['name' => 'Zyrtec'],
+            ['name' => 'Metformin'],
+        ];
+
+        Medicine::insert($medicines);
     }
 }
